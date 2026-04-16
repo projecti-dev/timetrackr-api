661d12b840bd7be38028d7eeffe1cc13f1ab563d
projecti-dev <mihri@printnest.com>
2026-04-16 12:58:19 -0500

    feat: auth module + JWT login + seed users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
 
 @Module({
@@ -22,6 +23,7 @@ import { User } from './users/user.entity';
       }),
     }),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
