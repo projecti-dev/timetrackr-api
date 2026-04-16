835c61623f857761872f6fca1138f5af97ff849e
projecti-dev <Amanmyradovamahri22@gmail.com>
2026-04-16 13:28:10 -0500

    feat: database config + User entity + Users module

---
+++ src/users/user.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  displayName!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
